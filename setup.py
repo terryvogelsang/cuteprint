0e4582539fb4621b8cc5f953c1a985f0b9477c78
Terry Vogelsang <terryvogelsang@gmail.com>
2018-04-22 23:25:46 +0200

    Change structure

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 setup(
   name = 'cuteprint',
   packages = ['cuteprint'], # This must be the same as the name above
-  version = '0.2',
+  version = '0.1.1',
   description = 'A Pretty Print Utils Library',
   long_description = 'A Pretty Print Utils Library',
   author = 'Terry Vogelsang',
   author_email = 'terry.vogelsang@protonmail.com',
   url = 'https://github.com/terryvogelsang/cuteprint',
-  download_url = 'https://github.com/terryvogelsang/cuteprint/archive/0.1.tar.gz',
+  download_url = 'https://github.com/terryvogelsang/cuteprint/archive/0.1.1.tar.gz',
   keywords = ['pretty', 'print', 'cute'],
   classifiers = [],
 )
